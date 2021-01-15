a890df32a5f3c22e344d3e5ff88a4047d6ba1752
Lu-desu <luciano.providel@gmail.com>
2021-01-15 18:22:23 -0300

    Movimientos del cangrejo listos

--- Scenes/Crabby.gd
+++ Scenes/Crabby.gd
@@ -1,19 +1,32 @@
 extends KinematicBody2D
 
 var linear_vel = Vector2()
+var gravity = 100
 
 onready var playback = $AnimationTree.get("parameters/playback")
 
 func _physics_process(delta):
-	var target_vel = Input.get_action_strength("right") - Input.get_action_strength("left")
-	linear_vel.x = lerp(linear_vel.x,target_vel * 400,0.5)
-	linear_vel = move_and_slide(linear_vel)
-	if linear_vel.length_squared()>10:
-		playback.travel("Run")
+	linear_vel.y += gravity + delta
+	linear_vel = move_and_slide(linear_vel, Vector2.UP)
+	var on_floor = is_on_floor()
+	var target_vel = Vector2(
+		Input.get_action_strength("right") 
+		- Input.get_action_strength("left"), 0)
+	linear_vel.x = lerp(linear_vel.x,target_vel.x * 400,0.5)
+	if on_floor and Input.is_action_just_pressed("jump"):
+		linear_vel.y -= 1500
+	if on_floor:
+		if linear_vel.length_squared()>10:
+			playback.travel("Run")
+		else:
+			playback.travel("Idle")
 	else:
-		playback.travel("Idle")
+		if linear_vel.y > 0:
+			playback.travel("Fall")
+		else:
+			playback.travel("Jump_stay")
 	if linear_vel.x > 0:
 		$Sprite.flip_h = true
-	else:
+	if linear_vel.x < 0:
 		$Sprite.flip_h = false
 	
